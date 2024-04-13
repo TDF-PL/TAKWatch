b16d4f8d36076a88ad7d42e16d10019097d67673
TDF-PL-042 <TDF-PL-042@>
2024-04-13 13:56:44 +0200

    Sending status heart beat message to device

--- app/src/main/java/pl/tdf/atak/TAKWatch/TakWatchMapComponent.java
+++ app/src/main/java/pl/tdf/atak/TAKWatch/TakWatchMapComponent.java
@@ -48,6 +48,8 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import gov.tak.api.widgets.IMapWidget;
 import gov.tak.platform.ui.MotionEvent;
@@ -58,6 +60,7 @@ import pl.tdf.atak.TAKWatch.plugin.HeartRatePreferenceFragment;
 import pl.tdf.atak.TAKWatch.plugin.R;
 import pl.tdf.atak.TAKWatch.plugin.TAKWatchConst;
 import pl.tdf.atak.TAKWatch.radialmenu.RadialMenuDetailsExtender;
+import pl.tdf.atak.TAKWatch.statushb.StatusHeartBeatTask;
 
 
 public class TakWatchMapComponent extends AbstractMapComponent {
@@ -84,6 +87,9 @@ public class TakWatchMapComponent extends AbstractMapComponent {
     private long lastBroadcasted = System.currentTimeMillis();
 
     private WatchClient watchClient;
+
+    private Timer heartBeatConnectionStatusTimer;
+
     private final ConnectIQ.IQApplicationEventListener mapEventsListener = (iqDevice, iqApp, messages, iqMessageStatus) -> {
         if (messageDebouncer.alreadyHandled(messages)) {
             Log.d(TAG, "onMessageReceived - message '" + messages + "' already handled skipping: ");
@@ -361,6 +367,8 @@ public class TakWatchMapComponent extends AbstractMapComponent {
                 watchClient.reload();
             }
         });
+
+        registerStatusHeartBeat();
     }
 
     @Override
@@ -373,5 +381,22 @@ public class TakWatchMapComponent extends AbstractMapComponent {
         watchClient.cleanup();
 
         AtakBroadcast.getInstance().unregisterReceiver(openPreferencesReceiver);
+
+        unregisterStatusHeartBeat();
+    }
+
+    private void registerStatusHeartBeat() {
+        Log.d(TAG, "Registering status heart beat timer");
+        StatusHeartBeatTask task = new StatusHeartBeatTask(watchClient);
+        heartBeatConnectionStatusTimer = new Timer();
+        heartBeatConnectionStatusTimer.schedule(task, 5000, 5000);
+    }
+
+    private void unregisterStatusHeartBeat() {
+        Log.d(TAG, "Unregistering status heart beat timer");
+        if (heartBeatConnectionStatusTimer != null) {
+            heartBeatConnectionStatusTimer.purge();
+            heartBeatConnectionStatusTimer.cancel();
+        }
     }
 }
